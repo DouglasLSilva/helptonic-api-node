2af5dabefd00c0acd93ac1fce63e7e746a463ae0
Luis Rodrigues <49655479+lhmrodrigues@users.noreply.github.com>
2021-03-21 18:35:13 -0300

    [test] cor fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ var ambiente = process.env.NODE_ENV || 'development';
 
 
 app.use(express.urlencoded());
+
+if (ambiente === 'development'){
+    app.use(cors());
+}
+
 app.use(express.json());
 app.use(routes);
 
@@ -16,9 +21,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger_output.json');
 
 
-if (ambiente === 'development'){
-    app.use(cors());
-}
 
 var serverAPI = app.listen(apiPort, function () {
   console.log('Server API listening at http://' + serverAPI.address().address + ':' + serverAPI.address().port);
